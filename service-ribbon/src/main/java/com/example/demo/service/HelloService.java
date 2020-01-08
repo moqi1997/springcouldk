119fa57a8a068b84b0b9ab7984f510b8ff07ec9d
moqi <1176323390@qq.com>
2020-01-08 10:50:17 +0800

    springcould Finchley 版demo(hystrix)

--- service-ribbon/src/main/java/com/example/demo/service/HelloService.java
+++ service-ribbon/src/main/java/com/example/demo/service/HelloService.java
@@ -1,5 +1,6 @@
 package com.example.demo.service;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
@@ -8,8 +9,12 @@ import org.springframework.web.client.RestTemplate;
 public class HelloService {
     @Autowired
     RestTemplate restTemplate;
-
+    @HystrixCommand(fallbackMethod = "hiError")
     public String hiService(String name) {
         return restTemplate.getForObject("http://SERVICE-HI/hi?name="+name,String.class);
+    }
+
+    public String hiError(String name) {
+        return "我的,"+name+",sorry,error!";
     }
 }
